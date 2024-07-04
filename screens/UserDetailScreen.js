3819188b38a35eebbd369c58c7d783442b0e0dd8
Leandro Licata <leandrolicata1@gmail.com>
2024-07-04 18:07:24 -0300

    trabajando en user detail

--- screens/UserDetailScreen.js
+++ screens/UserDetailScreen.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
+import { database } from "../database/firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
 
-const UserDetailScreen = () => {
+const UserDetailScreen = (props) => {
+  const getUserById = async (id) => {
+    const docRef = doc(database, "users", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getUserById(props.route.params.userId);
+  }, []);
+
   return (
     <View>
       <Text>User Detail Screen</Text>
+      <Text>{props.route.params.userId}</Text>
     </View>
   );
 };
--- screens/UserList.js
+++ screens/UserList.js
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView, Button } from "react-native";
 import { database } from "../database/firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { ListItem, Avatar } from "@rneui/themed";
 
 const UserList = (props) => {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(database, "users"));
-        const usersList = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setUsers(usersList);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    };
+  const fetchUsers = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(database, "users"));
+      const usersList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setUsers(usersList);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchUsers();
   }, []);
 
@@ -29,6 +30,29 @@ const UserList = (props) => {
         title="Create User"
         onPress={() => props.navigation.navigate("CreateUserScreen")}
       />
+      {users.map((user) => {
+        return (
+          <ListItem
+            key={user.id}
+            bottomDivider
+            onPress={() => {
+              props.navigation.navigate("UserDetailScreen", {
+                userId: user.id,
+              });
+            }}
+          >
+            <ListItem.Chevron />
+            <Avatar
+              rounded
+              source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
+            />
+            <ListItem.Content>
+              <ListItem.Title>{user.name}</ListItem.Title>
+              <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        );
+      })}
     </ScrollView>
   );
 };
